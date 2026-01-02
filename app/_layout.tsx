75b7636d4a45c9f9e706b7e5d44a6c002471382b
Bryan Mangapit <bryanjesusmangapit477@gmail.com>
2026-01-02 15:34:59 +0800

    Initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import '../global.css';
+import { Stack } from 'expo-router';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          animation: 'fade_from_bottom',
+        }}
+      />
+    </SafeAreaProvider>
+  );
+}

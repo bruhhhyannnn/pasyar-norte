48c14ec5854b66bc187ce8070fff300ba6e4fc3d
Bryan Mangapit <bryanjesusmangapit477@gmail.com>
2026-01-03 09:40:00 +0800

    style(ui): Add themed view to custom category card component for dynamic theme

--- src/components/ui/CustomCategoryCard.tsx
+++ src/components/ui/CustomCategoryCard.tsx
@@ -1,6 +1,7 @@
 import { Alert, TouchableOpacity } from 'react-native';
 import Icon from './Icon';
 import ThemedText from './ThemedText';
+import ThemedView from './ThemedView';
 
 interface CustomCategoryProps {
   category: string;
@@ -10,12 +11,15 @@ interface CustomCategoryProps {
 export default function CustomCategory({ category, iconName }: CustomCategoryProps) {
   return (
     <TouchableOpacity
-      className="flex-row flex-wrap items-center gap-2 rounded-2xl border border-slate-400 bg-slate-50 px-3 py-2"
       onPress={() => {
         Alert.alert(`${category} category clicked!`);
       }}>
-      <Icon name={iconName} />
-      <ThemedText variant="body_lg">{category}</ThemedText>
+      <ThemedView
+        variant="bg_light"
+        className="flex-row flex-wrap items-center gap-2 rounded-2xl border border-slate-400 px-3 py-2">
+        <Icon name={iconName} />
+        <ThemedText variant="body_lg">{category}</ThemedText>
+      </ThemedView>
     </TouchableOpacity>
   );
 }

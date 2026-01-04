5590a0a54eac6a4693009e6ad01bf5d8f250f860
Bryan Mangapit <bryanjesusmangapit477@gmail.com>
2026-01-04 22:37:10 +0800

    style(ui): Add dynamic changing theme for shadows

--- src/styles/shadow.ts
+++ src/styles/shadow.ts
@@ -1,11 +1,15 @@
 // Using custom shadow since tailwind is not compatible with shadows natively.
+
+import { useThemeStore } from '@/stores/useThemeStore';
 import { Platform, StyleSheet } from 'react-native';
 
 export function useShadows() {
+  const theme = useThemeStore((state) => state.theme);
+
   return StyleSheet.create({
     card: {
       // TODO: use actual external and unified color palette here
-      shadowColor: '#1e293b',
+      shadowColor: theme === 'light' ? '#1e293b' : '#94a3b8',
       shadowOffset: { width: 0, height: 4 },
       shadowOpacity: Platform.OS === 'ios' ? 0.18 : 0.12,
       shadowRadius: Platform.OS === 'ios' ? 10 : 6,

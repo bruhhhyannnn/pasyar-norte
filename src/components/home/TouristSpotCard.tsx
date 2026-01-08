aa3956b7e57ce9ae11c14a2df88a7935fd53764d
Bryan Mangapit <bryanjesusmangapit477@gmail.com>
2026-01-08 14:57:57 +0800

    style(ui): Create TouristSpotCard reusable component for tourist spot contents

---
+++ src/components/home/TouristSpotCard.tsx
@@ -0,0 +1,25 @@
+import { View, ScrollView } from 'react-native';
+import { ThemedText, ThemedView, IconProps } from '@/components/ui';
+import CustomCategoryCard from '@/components/home/CustomCategoryCard';
+
+interface TouristSpotCardProps {
+  name: string;
+  description: string;
+  icon: IconProps;
+}
+
+export default function TouristSpotCard({ name, description, icon }: TouristSpotCardProps) {
+  return (
+    <View className="px-12 py-1">
+      <ThemedView variant="bg_light" className="h-full rounded-xl p-3">
+        <ScrollView>
+          <View className="flex-row items-center gap-2 ">
+            <ThemedText variant="h300">{name}</ThemedText>
+            <CustomCategoryCard category="test" iconName="image-outline" />
+          </View>
+          <ThemedText>{description}</ThemedText>
+        </ScrollView>
+      </ThemedView>
+    </View>
+  );
+}
